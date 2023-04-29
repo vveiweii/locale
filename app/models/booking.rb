af6704198dcfbbc5732c73e17a87f534bbe80fe3
Rafael Moreira <rafamsilva@gmail.com>
2023-04-29 15:35:21 +1000

    updated migration files - removed belongs to cart from booking model - schema update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :cart
 
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :end_date, comparison: { greater_than: :start_date }
-
 end
--- db/migrate/20230429021123_create_bookings.rb
+++ db/migrate/20230429021123_create_bookings.rb
@@ -2,8 +2,6 @@ class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
       t.references :user, null: false, foreign_key: true
-      t.belongs_to :review, null: false, foreign_key: true
-      t.belongs_to :cart, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
 
