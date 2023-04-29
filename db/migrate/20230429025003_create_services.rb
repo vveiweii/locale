ee3af76ada0229dcde4318dcab754c31930075d8
Fitore Sadiku <fitore.sadiku@hotmail.com>
2023-04-29 14:09:28 +1000

    Created Services Model and Controller, have not run db migrate

---
+++ db/migrate/20230429025003_create_services.rb
@@ -0,0 +1,15 @@
+class CreateServices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :services do |t|
+      t.references :business, null: false, foreign_key: true
+      t.string :name
+      t.float :price
+      t.text :description
+      t.text :offer
+      t.text :available
+      t.references :cart, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
