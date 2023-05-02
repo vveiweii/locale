8b5079fdd0434925dfbae7cba5701cedf295931e
Rafael Moreira <rafamsilva@gmail.com>
2023-05-02 20:19:00 +1000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
   get 'dashboard', to: 'pages#userdashboard'
 
-  resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :bookings, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-    resources :businesses, only: [:new, :create, :edit, :update, :destroy] do
-      resources :services, only: [:new, :create, :edit, :update, :destroy]
+  namespace :users do
+    resource :profile, only: %i[show edit update destroy]
+    resources :bookings
+    resources :businesses do
+      resources :services
     end
   end
 
-  resources :businesses, only: [:index, :show] do
+  resources :businesses, only: %i[index show] do
     resources :services, only: [:index]
   end
+
+  # resource :booking_confirmation, only: [:show]
 end
