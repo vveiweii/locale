bee90ad12d21aee8e64ed600d792962bc61fbb39
Rafael Moreira <rafamsilva@gmail.com>
2023-05-02 20:35:40 +1000

    models updates - validations - removal of CArt validation

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,9 @@
 class Business < ApplicationRecord
   belongs_to :user
 
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 500 }
+
   has_many :services, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+
   has_many :businesses, dependent: :destroy
   has_many :reviews
   has_many :bookings, dependent: :destroy
