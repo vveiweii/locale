e4f55f8aabf9b5c08f9510a99f40b68f820e1004
Rafael Moreira <rafamsilva@gmail.com>
2023-05-10 12:42:05 +1000

    updated services controller - edit action - update action - params

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -9,17 +9,18 @@ class ServicesController < ApplicationController
 
   def create
     @service = Service.business.new(service_params)
-
     @service.save
     redirect_to business_path(@service.business)
   end
 
   def edit
-
+    @service = Service.find(params[:id])
   end
 
   def update
-
+    @service = Service.find(params[:id])
+    @service.update(service_params)
+    redirect_to business_path(@service.business)
   end
 
   def destroy
@@ -31,6 +32,6 @@ class ServicesController < ApplicationController
   private
 
   def service_params
-    params.require(:service).permit(:name, :description, :price, :offer, :available, :business_id)
+    params.require(:service).permit(:name, :description, :price, :offer, :available)
   end
 end
