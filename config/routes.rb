082d0bd0fe1db7d33ac9911d3066e2dd29518a7d
Rafael Moreira <rafamsilva@gmail.com>
2023-05-10 12:38:39 +1000

    updated routes - for cart - for line items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root to: "pages#home"
 
   resources :dashboard, only: %i[index update destroy]
@@ -17,6 +16,16 @@ Rails.application.routes.draw do
   resources :businesses, only: %i[index show] do
     resources :services, only: %i[index]
   end
+
+  # routes related to cart + line_items + booking confirmation
+  get 'carts/:id' => "carts#show", as: "cart"
+  delete 'carts/:id' => "carts#destroy"
+
+  post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
+  post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
+  post 'line_items' => "line_items#create"
+  get 'line_items/:id' => "line_items#show", as: "line_item"
+  delete 'line_items/:id' => "line_items#destroy"
 
   # resource :booking_confirmation, only: [:show]
 end
