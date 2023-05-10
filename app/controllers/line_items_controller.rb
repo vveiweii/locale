ebb355bc72d562e4b479ec23069ef8f48e24e832
Rafael Moreira <rafamsilva@gmail.com>
2023-05-10 13:09:56 +1000

    updated line_items model+controller - added total_price and taxes helpers on model - logic inside line_items controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,13 +1,45 @@
+# rubocop:disable Metrics/MethodLength
 class LineItemsController < ApplicationController
   def create
+    chosen_product = Service.find(params[:service_id])
+    current_cart = @current_cart
+
+    if current_cart.products.include?(chosen_product)
+      @line_item = current_cart.line_items.find_by(service_id: chosen_product)
+      @line_item.quantity += 1
+    else
+      @line_item = LineItem.new
+      @line_item.cart = current_cart
+      @line_item.service = chosen_product
+    end
+    @line_item.save
+    redirect_to cart_path(current_cart)
   end
 
   def add_quantity
+    @line_item = LineItem.find(params[:id])
+    @line_item.quantity += 1
+    @line_item.save
+    redirect_to cart_path(@current_cart)
   end
 
   def reduce_quantity
+    @line_item = LineItem.find(params[:id])
+    @line_item.quantity -= 1 if @line_item.quantity > 1
+    @line_item.save
+    redirect_to cart_path(@current_cart)
   end
 
   def destroy
+    @line_item = LineItem.find(params[:id])
+    @line_item.destroy
+    redirect_to cart_path(@current_cart)
+  end
+
+  private
+
+  def line_item_params
+    params.require(:line_item).permit(:quantity, :service_id, :cart_id)
   end
 end
+# rubocop:enable Metrics/MethodLength
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,12 @@ class LineItem < ApplicationRecord
   belongs_to :service
   belongs_to :cart
   belongs_to :booking
+
+  def total_price
+    self.quantity * self.service.price
+  end
+
+  def taxes
+    self.service.price * 0.1
+  end
 end
