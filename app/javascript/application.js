7bb0c17323ab0e8e4d8341ab89819ce1d8fb4800
Rafael Moreira <rafamsilva@gmail.com>
2023-05-11 11:49:25 +1000

    removed swiper footmarks

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "bootstrap"
-import 'swiper/css/bundle'
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "bootstrap";
