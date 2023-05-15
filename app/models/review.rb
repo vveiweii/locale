33834cae4c3a70b754d2b1bf758e32e98a8429de
Rafael Moreira <rafamsilva@gmail.com>
2023-05-15 13:46:16 +1000

    updated review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
+  belongs_to :booking
 
   validates :description, presence: true, length: { maximum: 500 }
   validates :rating, numericality: true, presence: true, inclusion: { in: 1..5 }
