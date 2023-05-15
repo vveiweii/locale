204d64dae1b650695af2237f6c4219c3f55bfba3
Rafael Moreira <rafamsilva@gmail.com>
2023-05-15 12:20:46 +1000

    updated authentication for sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?, except: [:home, :about, :index, :show]
-  before_action :authenticate_user!, only: %i[new create], if: -> { params[:business_id].present? }
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!, only: %i[new create], if: -> { params[:business_id].present? }
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   before_action :current_cart
@@ -23,6 +23,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :photo)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :photo) }
   end
 end
