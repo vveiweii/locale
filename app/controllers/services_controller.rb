d06ff0919c0f876deda84805f3576c88c675a0cc
Rafael Moreira <rafamsilva@gmail.com>
2023-05-18 20:32:30 +1000

    New service inside bizdash working - added create action code on ServicesController - Updated model, removing validation, adding default attribute - new service modal - rende new service line

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,13 +4,17 @@ class ServicesController < ApplicationController
   end
 
   def new
-    @service = Service.business.new
+    @service = Service.new
   end
 
   def create
-    @service = Service.business.new(service_params)
-    @service.save
-    redirect_to business_path(@service.business)
+    @service = Service.new(service_params)
+    @service.business_id = params[:business_id]
+    if @service.save
+      redirect_to request.referer, notice: 'Service was successfully created.'
+    else
+      redirect_to request.referer, alert: 'Service was not created.'
+    end
   end
 
   def edit
@@ -26,12 +30,12 @@ class ServicesController < ApplicationController
   def destroy
     @service = Service.find(params[:id])
     @service.destroy
-    redirect_to business_path(@service.business)
+    redirect_to request.referer, notice: 'Service was successfully deleted.'
   end
 
   private
 
   def service_params
-    params.require(:service).permit(:name, :description, :price, :offer, :available)
+    params.require(:service).permit(:name, :description, :price, :offer)
   end
 end
