4255a461b6904269099b56f42aa2d4f1eae75430
Rafael Moreira <rafamsilva@gmail.com>
2023-05-18 12:23:07 +1000

    Biz Dashboard creation and basic config

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :dashboard, only: %i[index update destroy]
+  resources :dashboard, only: %i[index]
+  get 'bizdashboard/index', as: 'bizdashboard'
 
   resources :users, only: %i[edit update destroy] do
     resources :bookings, only: %i[new create edit update] do
@@ -20,7 +21,7 @@ Rails.application.routes.draw do
     resources :services, only: %i[index destroy]
   end
 
-  # routes related to cart + line_items + booking confirmation
+  # routes related to cart + line_items
   get 'carts/:id' => "carts#show", as: "cart"
   delete 'carts/:id' => "carts#destroy"
 
@@ -29,8 +30,6 @@ Rails.application.routes.draw do
   post 'line_items' => "line_items#create"
   get 'line_items/:id' => "line_items#show", as: "line_item"
   delete 'line_items/:id' => "line_items#destroy"
-
-  resource :booking_confirmation, only: [:show]
 
   # route related to the for your business page
 
