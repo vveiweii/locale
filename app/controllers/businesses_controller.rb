8fdf285a1a69219c9109ecf477e9a0be2ce3eb42
Rafael Moreira <rafamsilva@gmail.com>
2023-05-23 09:52:06 +1000

    - minor refac - Added method near (Melbourne) when searching

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,13 +1,18 @@
 class BusinessesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_business, only: %i[show update destroy]
 
+  # rubocop:disable Metrics
   def index
+    # @user_location = request.location
     if params[:query].present?
-      @businesses = Business.global_search(params[:query]).where(available: 'yes')
+      @businesses = Business.global_search(params[:query])
+                            .where(available: 'yes')
+                            .near('Melbourne, Australia', 50)
     else
-      @businesses = Business.all
+      @businesses = Business.all.where(available: 'yes').near('Melbourne, Australia', 50)
     end
+
     @markers = @businesses.map do |business|
       {
         lat: business.latitude,
@@ -16,6 +21,7 @@ class BusinessesController < ApplicationController
       }
     end
   end
+  # rubocop:enable Metrics
 
   def new
     @business = Business.new
