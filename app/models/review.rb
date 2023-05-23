94a0c64762271deec9d31b4def983ac765426b78
Rafael Moreira <rafamsilva@gmail.com>
2023-05-23 11:48:56 +1000

    Added Review.title field - db:migrate to update schema - updated model - Added title to views - added title to forms - Updated controller params

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :booking
-  #belongs_to :business
 
   validates :description, presence: true, length: { maximum: 500 }
   validates :rating, numericality: true, presence: true, inclusion: { in: 1..5 }
+  validates :title, presence: true, length: { maximum: 44 }
 end
